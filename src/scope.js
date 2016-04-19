07b0563fc4a560ab73f9f8ca9dbc75874d50e135
sheepjian <jerrydeng.windfly@gmail.com>
2016-04-19 22:06:16 +0800

    optimize the digest design of angular scope

--- src/scope.js
+++ src/scope.js
@@ -5,27 +5,81 @@ function Scope() {
 	this.$$watchers = [];
 }
 
-Scope.prototype.dirtyCheck = function(oldValue, newValue) {
-	return oldValue !== newValue;
-};
+var initWatchVal = function() {};
 
-Scope.prototype.$digest = function() {
+Scope.prototype.$$superDigestOnce = function() {
 	var self = this;
-	var newValue, oldValue;
+	var newValue, oldValue, dirty;
+	var cleanWatcher = [];
+	var dirtyWatcher = [];
 	_.forEach(this.$$watchers, function(watcher) {
 		newValue = watcher.watchFn(self);
 		oldValue = watcher.last;
 		if(self.dirtyCheck(newValue, oldValue)) {
 			watcher.last = newValue;
+			if(oldValue === initWatchVal) {
+				oldValue = newValue;
+			}
 			watcher.listenFn(newValue, oldValue, self);
+			dirty = true;
 		}
 	});
+	_.forEach(this.$$watchers, function(watcher) {
+		newValue = watcher.watchFn(self);
+		oldValue = watcher.last;
+		if(self.dirtyCheck(newValue, oldValue)) {
+			dirtyWatcher.push(watcher);
+		} else {
+			cleanWatcher.push(watcher);
+		}
+	});
+	this.$$watchers = cleanWatcher.concat(dirtyWatcher);
+	return dirty;
+};
+
+Scope.prototype.dirtyCheck = function(oldValue, newValue) {
+	return oldValue !== newValue;
+};
+
+Scope.prototype.$$digestOnce = function() {
+	var self = this;
+	var newValue, oldValue, dirty;
+	_.forEach(this.$$watchers, function(watcher) {
+		newValue = watcher.watchFn(self);
+		oldValue = watcher.last;
+		if(self.dirtyCheck(newValue, oldValue)) {
+			watcher.last = newValue;
+			if(oldValue === initWatchVal) {
+				oldValue = newValue;
+			}
+			watcher.listenFn(newValue, oldValue, self);
+			dirty = true;
+		}
+	});
+	return dirty;
+};
+
+Scope.prototype.$digest = function(isSuperDigest) {
+	var dirty;
+	var counter = 0;
+	do {
+		if(isSuperDigest)
+			dirty = this.$$superDigestOnce();
+		else
+			dirty = this.$$digestOnce();
+		counter++;
+	} while (dirty && counter<=10);
+	if(dirty)
+		throw "10 digest iterations reached";
+
+	return counter;
 };
 
 Scope.prototype.$watch = function(watchFn, listenFn) {
 	var watcher = {
 		watchFn: watchFn,
-		listenFn: listenFn
+		listenFn: listenFn || function() {},
+		last: initWatchVal
 	};
 
 	this.$$watchers.push(watcher);
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -2,14 +2,14 @@
 
 var Scope = require('../src/scope');
 
-describe("Scope", function() {
+describe("Scope--->", function() {
 	it("can be constructed and used as an object", function() {
 		var scope = new Scope();
 		scope.aProperty = 1;
 		expect(scope.aProperty).toBe(1);
 	});
 
-	describe("digest", function() {
+	describe("digest--->", function() {
 		var scope;
 
 		beforeEach(function() {
@@ -67,6 +67,156 @@ describe("Scope", function() {
 
 				scope.$digest();
 				expect(scope.counter).toBe(2);
-			})
+			});
+
+		it("calls listener when watch value is first undefined", function() {
+			scope.counter = 0;
+			scope.$watch(
+				function(scope) {
+					return scope.someValue;
+				},
+				function(newValue, oldValue, scope) {
+					scope.counter++;
+				}
+			);
+			scope.$digest();
+			expect(scope.counter).toBe(1);
+		});
+
+		it("calls listener with new value as old value the first time", function() {
+			scope.someValue = 123;
+			var oldValueGiven;
+			scope.$watch(
+				function(scope) {
+					return scope.someValue;
+				},
+				function(newValue, oldValue, scope) {
+					oldValueGiven = oldValue;
+				}
+			);
+			scope.$digest();
+			expect(oldValueGiven).toBe(123);
+		});
+
+		it("may have watchers that omit the listener function", function() {
+			var watchFn = jasmine.createSpy().and.returnValue('something');
+			scope.$watch(watchFn);
+			scope.$digest();
+			expect(watchFn).toHaveBeenCalled();
+		});
+
+		it("triggers chained watchers in the same digest", function() {
+			scope.name = 'Jane';
+
+			// this order of watchers will call the digest function three times
+			// reverse the order, the calling time will be 2
+			// this may cost very much when updating the watcher of the DOM
+			// reactiveJS solve this issue to compute the equilibrium state in the memory
+			// TODO: find the optimal way to order the watchers 
+			// Idea: use truth table for building the topological order 
+			// How to prove the topological order is the optimal updating strategy? 
+			scope.$watch(
+				function(scope) {
+					return scope.nameUpper;
+				},
+				function(newValue, oldValue, scope) {
+					if (newValue) {
+						scope.initial = newValue.substring(0, 1) + '.';
+					}
+				}
+			);
+			scope.$watch(
+				function(scope) {
+					return scope.name;
+				},
+				function(newValue, oldValue, scope) {
+					if (newValue) {
+						scope.nameUpper = newValue.toUpperCase();
+					}
+				}
+			);
+			scope.$digest();
+			expect(scope.initial).toBe('J.');
+			scope.name = 'Bob';
+			scope.$digest();
+			expect(scope.initial).toBe('B.');
+		});
+
+		it("gives up on the watches after 10 iterations", function() {
+			scope.counterA = 0;
+			scope.counterB = 0;
+			scope.$watch(
+				function(scope) {
+					return scope.counterA;
+				},
+				function(newValue, oldValue, scope) {
+					scope.counterB++;
+				}
+			);
+			scope.$watch(
+				function(scope) {
+					return scope.counterB;
+				},
+				function(newValue, oldValue, scope) {
+					scope.counterA++;
+				}
+			);
+			expect((function() {
+				scope.$digest();
+			})).toThrow();
+		});
+
+		it("test the superDigestOnce method", function() {
+			scope.a = 0;
+			scope.b = 1;
+			scope.c = 2;
+			scope.d = 4;
+
+			scope.$watch(
+				function(scope) {
+					return scope.d;
+				},
+				function(newValue, oldValue, scope) {
+				}
+			);
+			scope.$watch(
+				function(scope) {
+					return scope.c;
+				},
+				function(newValue, oldValue, scope) {
+					scope.d++;
+				}
+			);
+			scope.$watch(
+				function(scope) {
+					return scope.b;
+				},
+				function(newValue, oldValue, scope) {
+					scope.c++;
+				}
+			);
+			scope.$watch(
+				function(scope) {
+					return scope.a;
+				},
+				function(newValue, oldValue, scope) {
+					scope.b++;
+				}
+			);
+			var digestRound = scope.$digest(false);
+			//console.log('Digest Rounds with no optimization is '+digestRound);
+
+			scope.a = 1;
+			var optimizingDigestRound = scope.$digest(true);
+			//console.log('Digest Rounds during optimization is '+digestRound);
+
+			scope.a = 2;
+			var optimizedDigestRound = scope.$digest(true);
+			//console.log('Digest Rounds after optimization is '+digestRound);
+
+			expect(optimizingDigestRound>=optimizedDigestRound).toBe(true);
+		});
+
+
 	});
 });
