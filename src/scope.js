5f2469953eaae3b8552f8d35636b11243c06c732
sheepjian <jerrydeng.windfly@gmail.com>
2016-04-21 23:05:16 +0800

    phase day 3

--- src/scope.js
+++ src/scope.js
@@ -4,6 +4,8 @@ var _ = require('lodash');
 function Scope() {
 	this.$$watchers = [];
 	this.$$lastDirtyWatch = null;
+	this.$$asyncQueue = [];
+	this.$$phase = null;
 }
 
 var initWatchVal = function() {};
@@ -16,9 +18,9 @@ Scope.prototype.$$superDigestOnce = function() {
 	_.forEach(this.$$watchers, function(watcher) {
 		newValue = watcher.watchFn(self);
 		oldValue = watcher.last;
-		if(self.dirtyCheck(newValue, oldValue)) {
+		if (self.dirtyCheck(newValue, oldValue)) {
 			watcher.last = newValue;
-			if(oldValue === initWatchVal) {
+			if (oldValue === initWatchVal) {
 				oldValue = newValue;
 			}
 			watcher.listenFn(newValue, oldValue, self);
@@ -28,7 +30,7 @@ Scope.prototype.$$superDigestOnce = function() {
 	_.forEach(this.$$watchers, function(watcher) {
 		newValue = watcher.watchFn(self);
 		oldValue = watcher.last;
-		if(self.dirtyCheck(newValue, oldValue)) {
+		if (self.dirtyCheck(newValue, oldValue)) {
 			dirtyWatcher.push(watcher);
 		} else {
 			cleanWatcher.push(watcher);
@@ -39,11 +41,11 @@ Scope.prototype.$$superDigestOnce = function() {
 };
 
 Scope.prototype.dirtyCheck = function(oldValue, newValue, valueEq) {
-	if((typeof newValue === 'number' && typeof oldValue === 'number' &&
-isNaN(newValue) && isNaN(oldValue)))
+	if ((typeof newValue === 'number' && typeof oldValue === 'number' &&
+			isNaN(newValue) && isNaN(oldValue)))
 		return false;
 
-	if(valueEq) {
+	if (valueEq) {
 		return !_.isEqual(oldValue, newValue);
 	} else {
 		return (oldValue !== newValue);
@@ -55,18 +57,18 @@ Scope.prototype.$$digestOnce = function() {
 	var newValue, oldValue, dirty;
 	var currentWatchers;
 
-	_.forEach(self.$$watchers , function(watcher) {
+	_.forEach(self.$$watchers, function(watcher) {
 		newValue = watcher.watchFn(self);
 		oldValue = watcher.last;
-		if(self.dirtyCheck(newValue, oldValue, watcher.valueEq)) {
+		if (self.dirtyCheck(newValue, oldValue, watcher.valueEq)) {
 			self.$$lastDirtyWatch = watcher;
 			watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
-			if(oldValue === initWatchVal) {
+			if (oldValue === initWatchVal) {
 				oldValue = newValue;
 			}
 			watcher.listenFn(newValue, oldValue, self);
 			dirty = true;
-		} else if(self.$$lastDirtyWatch === watcher) {
+		} else if (self.$$lastDirtyWatch === watcher) {
 			return false;
 		}
 	});
@@ -78,16 +80,21 @@ Scope.prototype.$digest = function(isSuperDigest) {
 	var counter = 0;
 	var upperBound = 10;
 	this.$$lastDirtyWatch = null;
+	this.$beginPhase('$digest');
 	do {
-		if(isSuperDigest)
+		while (this.$$asyncQueue.length) {
+			var asyncTask = this.$$asyncQueue.shift();
+			this.$eval(asyncTask);
+		}
+		if (isSuperDigest)
 			dirty = this.$$superDigestOnce();
 		else
 			dirty = this.$$digestOnce();
 		counter++;
-	} while (dirty && counter<=upperBound);
-	if(dirty)
+	} while ((dirty || this.$$asyncQueue.length) && counter <= upperBound);
+	if (dirty || this.$$asyncQueue.length)
 		throw upperBound + " digest iterations reached";
-
+	this.$clearPhase();
 	return counter;
 };
 
@@ -107,9 +114,36 @@ Scope.prototype.$eval = function(evalFn, args) {
 	return evalFn(this, args);
 };
 
-Scope.prototype.$apply = function(applyFn) {
-	this.$eval(applyFn);
-	this.$digest();
+Scope.prototype.$evalAsync = function(evalFn) {
+	var self = this;
+	if(!self.$$phase && !self.$$asyncQueue.length) {
+		setTimeout(function() {
+			self.$digest();
+		}, 0);
+	}
+	this.$$asyncQueue.push(evalFn);
 };
+
+Scope.prototype.$apply = function(applyFn) {
+	try {
+		this.$beginPhase('$apply');
+		this.$eval(applyFn);
+	} finally {
+		this.$clearPhase();
+		this.$digest();
+	}
+};
+
+Scope.prototype.$beginPhase = function(phase) {
+	if (this.$$phase) {
+		throw this.$$phase + ' already in progress.';
+	}
+	this.$$phase = phase;
+};
+
+Scope.prototype.$clearPhase = function() {
+	this.$$phase = null;
+};
+
 
 module.exports = Scope;
