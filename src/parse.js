eee4244e38c9e4fd3a62e5f1a0f72602de8bdd37
sheepjian <jerrydeng.windfly@gmail.com>
2016-05-03 23:35:03 +0800

    day 13 expression lexer

--- src/parse.js
+++ src/parse.js
@@ -1,20 +1,70 @@
 'use strict';
 
-function Lexer() {
+var _ = require('lodash');
 
-}
+var TOKEN = {
+    SPACE: 0,
+    INTEGER: 1,
+    DOUBLE: 2,
+    UNKNOWN: 3
+};
+
+var REGEXP = [{
+    pattern: /^\s+$/,
+    token: TOKEN.SPACE
+}, {
+    pattern: /^-?\d+$/,
+    token: TOKEN.INTEGER
+}, {
+    pattern: /^-?\d+\.\d*$/,
+    token: TOKEN.FLOAT
+}, {
+    pattern: /^.*$/,
+    token: TOKEN.UNKNOWN
+}];
+
+function Lexer() {}
 
 Lexer.prototype.lex = function(text) {
     // Tokenization will be done here
+    var tokens = [];
+    var textArray = text.match(/\S+/g);
+    _.forEach(textArray, function(t) {
+        for (var i = 0; i < REGEXP.length; i++) {
+            var reg = REGEXP[i];
+            if (reg.pattern.test(t)) {
+                tokens.push(reg.token);
+                break;
+            }
+        }
+    });
+    return tokens;
 };
 
 function AST(lexer) {
     this.lexer = lexer;
 }
 
+
+// build the tree node: {value:'', type:''}
 AST.prototype.ast = function(text) {
     this.tokens = this.lexer.lex(text);
     // AST building will be done here
+    if (this.tokens.length == 1) {
+        if (this.tokens[0] === TOKEN.INTEGER) {
+            return function() {
+                return parseInt(text);
+            };
+        } else if (this.tokens[0] === TOKEN.FLOAT) {
+            return function() {
+                return parseFloat(text);
+            };
+        }
+    }
+
+    return function() {
+        return null;
+    };
 };
 
 function ASTCompiler(astBuilder) {
@@ -24,6 +74,7 @@ function ASTCompiler(astBuilder) {
 ASTCompiler.prototype.compile = function(text) {
     var ast = this.astBuilder.ast(text);
     // AST compilation will be done here
+    return ast;
 };
 
 function Parser(lexer) {
@@ -36,13 +87,8 @@ Parser.prototype.parse = function(text) {
     return this.astCompiler.compile(text);
 };
 
-function parse(expr) {
-    return function() {
-        return expr;
-    };
-}
 
-function parse1(expr) {
+function parse(expr) {
     var lexer = new Lexer();
     var parser = new Parser(lexer);
     return parser.parse(expr);
--- test/parse_spec.js
+++ test/parse_spec.js
@@ -4,13 +4,13 @@ var parse = require('../src/parse');
 
 describe("parse-->", function() {
     it("can parse an integer", function() {
-        var fn = parse(42);
+        var fn = parse('42');
         expect(fn).toBeDefined();
         expect(fn()).toBe(42);
     });
 
     it("can parse a floating point number", function() {
-        var fn = parse(4.2);
+        var fn = parse('4.2');
         expect(fn()).toBe(4.2);
     });
 });
