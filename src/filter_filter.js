72b7351188452cd767c5c8ce11f57315619751dd
sheepjian <jerrydeng.windfly@gmail.com>
2016-05-26 23:35:01 +0800

    day 30 expression filter wildcard

--- src/filter_filter.js
+++ src/filter_filter.js
@@ -2,18 +2,6 @@
 
 var _ = require('lodash');
 
-function comparator(actual, expected) {
-  if (_.isUndefined(actual)) {
-    return false;
-  }
-  if (_.isNull(actual) || _.isNull(expected)) {
-    return actual === expected;
-  }
-  actual = ('' + actual).toLowerCase();
-  expected = ('' + expected).toLowerCase();
-  return actual.indexOf(expected) !== -1;
-}
-
 function deepCompare(actual, expected, comparator, matchAnyProperty) {
   if (_.isString(expected) && _.startsWith(expected, '!')) {
     return !deepCompare(actual, expected.substring(1), comparator, matchAnyProperty);
@@ -31,6 +19,11 @@ function deepCompare(actual, expected, comparator, matchAnyProperty) {
           if (_.isUndefined(expectedVal)) {
             return true;
           }
+          if (expectedKey === '$') {
+            return _.some(actual, function(actualVal) {
+              return deepCompare(actualVal, expectedVal, comparator, true);
+            });
+          }
           return deepCompare(actual[expectedKey], expectedVal, comparator);
         }
       );
@@ -47,14 +40,41 @@ function deepCompare(actual, expected, comparator, matchAnyProperty) {
 }
 
 
-function createPredicateFn(expression) {
+function createPredicateFn(expression, compareFn) {
+  var shouldMatchPrimitives =
+    _.isObject(expression) && ('$' in expression);
+
+  var comparator = compareFn;
+
+  if (!_.isFunction(comparator)) {
+    if (comparator === true) {
+      comparator = _.isEqual;
+    } else {
+      comparator = function(actual, expected) {
+        if (_.isUndefined(actual)) {
+          return false;
+        }
+        if (_.isNull(actual) || _.isNull(expected)) {
+          return actual === expected;
+        }
+
+        actual = ('' + actual).toLowerCase();
+        expected = ('' + expected).toLowerCase();
+        return actual.indexOf(expected) !== -1;
+      };
+    }
+  }
+
   return function predicateFn(item) {
+    if (shouldMatchPrimitives && !_.isObject(item)) {
+      return deepCompare(item, expression.$, comparator);
+    }
     return deepCompare(item, expression, comparator, true);
   };
 }
 
 function filterFilter() {
-  return function(array, filterExpr) {
+  return function(array, filterExpr, compareFn) {
     var predicateFn;
     if (_.isFunction(filterExpr)) {
       predicateFn = filterExpr;
@@ -63,7 +83,7 @@ function filterFilter() {
       _.isBoolean(filterExpr) ||
       _.isNull(filterExpr) ||
       _.isObject(filterExpr)) {
-      predicateFn = createPredicateFn(filterExpr);
+      predicateFn = createPredicateFn(filterExpr, compareFn);
     } else {
       return array;
     }
